cd5ebb383d50f35964fa206602cf1b98f91e9710
Helengendary <helenapicinin13@gmail.com>
2024-12-03 11:51:30 -0300

    Project Message criada

---
+++ demo/src/main/java/com/example/demo/model/ProjectMessageModel.java
@@ -0,0 +1,38 @@
+package com.example.demo.model;
+
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tbProjectMessage")
+public class ProjectMessageModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_project_message;
+
+    @Column
+    private String text;
+
+    @ManyToOne
+    @JoinColumn(name = "id_project")
+    private ProjectModel project;
+
+    // @OneToMany(mappedBy="project", orphanRemoval = true)
+    // private List<ProjectMessageModel> Projects;
+    
+    @ManyToOne
+    @JoinColumn(name = "id_interaction")
+    private InteractionModel interaction;
+
+    // @OneToMany(mappedBy="interaction", orphanRemoval = true)
+    // private List<ProjectMessageModel> Projects;
+}
