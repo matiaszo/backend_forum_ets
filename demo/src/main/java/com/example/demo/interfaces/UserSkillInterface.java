1c31dcec6796d6602edfa1d94240ff19057b0d15
Helengendary <helenapicinin13@gmail.com>
2024-12-06 08:55:28 -0300

    Começando o profile

---
+++ demo/src/main/java/com/example/demo/interfaces/UserSkillInterface.java
@@ -0,0 +1,5 @@
+package com.example.demo.interfaces;
+
+public interface UserSkillInterface {
+    public void Register(Long user, Long skill);
+}
---
+++ demo/src/main/java/com/example/demo/services/UserSkillService.java
@@ -0,0 +1,39 @@
+package com.example.demo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.demo.interfaces.UserSkillInterface;
+import com.example.demo.model.SkillsModel;
+import com.example.demo.model.UserModel;
+import com.example.demo.model.UserSkillModel;
+import com.example.demo.repositories.SkillsRepository;
+import com.example.demo.repositories.UserRepository;
+import com.example.demo.repositories.UserSkillRepository;
+
+public class UserSkillService implements UserSkillInterface {
+
+    @Autowired
+    UserRepository userRepo;
+
+    @Autowired
+    SkillsRepository skillRepo;
+
+    @Autowired
+    UserSkillRepository userSkillRepo;
+
+    @Override
+    public void Register(Long user, Long skill) {
+        UserSkillModel model = new UserSkillModel();
+
+        Optional<UserModel> userModel = userRepo.findById(user); 
+        model.setUser(userModel.get());
+        
+        Optional<SkillsModel> skillModel = skillRepo.findById(skill);
+        model.setSkill(skillModel.get());
+
+        userSkillRepo.save(model);
+    }
+    
+}
