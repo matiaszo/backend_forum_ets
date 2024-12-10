89f11503430ec58371a21fdaec8c5d82e2493af5
xmarimarquesh <marimarquesh39@gmail.com>
2024-12-10 09:53:59 -0300

    Security e Cors configurados

--- demo/src/main/java/com/example/demo/config/MyCorsConfiguration.java
+++ demo/src/main/java/com/example/demo/config/MyCorsConfiguration.java
@@ -1,15 +1,22 @@
+//tem que mudar o nome da classe (precisa ser diferente de CorsConfiguration)
+
 package com.example.demo.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 @Configuration
-public class CorsConfiguration implements WebMvcConfigurer {
+public class MyCorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
         registry.addMapping("/**")
-        .allowedOrigins("http://localhost:8080","http://127.0.0.1:5500")
+        .allowedOrigins("http://localhost:3000", "http://127.0.0.1:3000")
+        .allowedHeaders("Authorization", "*")
+        .allowCredentials(true)
         .allowedMethods(
             "GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT"
         );
