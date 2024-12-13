9db7468775c75973195f0151eb9aba7393adae05
OsvaldoComCH <eduhrsilva@gmail.com>
2024-12-13 10:12:47 -0300

    Removed unused imports

--- demo/src/main/java/com/example/demo/controller/ChatController.java
+++ demo/src/main/java/com/example/demo/controller/ChatController.java
@@ -1,6 +1,5 @@
 package com.example.demo.controller;
 
-import java.lang.classfile.ClassFile.Option;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Date;
--- demo/src/main/java/com/example/demo/controller/ProfileController.java
+++ demo/src/main/java/com/example/demo/controller/ProfileController.java
@@ -7,8 +7,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-import javax.swing.plaf.OptionPaneUI;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
